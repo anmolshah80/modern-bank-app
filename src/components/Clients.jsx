629f844fe1ae42b43b9a24e058a3acad4584ab8a
Anmol Shah <anmolsha80@gmail.com>
2024-09-27 19:46:10 +0545

    features/testimonials-clients-and-cta-section - Create a Testimonials, Clients, and CTA sections to render the clients' feedbacks, clients, and a call to action button to get started

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,5 +1,24 @@
-const Clients = () => {
-  return <div>Clients</div>;
-};
+import { clients } from '@/constants';
+
+import styles from '@/styles';
+
+const Clients = () => (
+  <section className={`${styles.flexCenter} gap-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map(({ id, logo }) => (
+        <div
+          key={id}
+          className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}
+        >
+          <img
+            src={logo}
+            alt="client"
+            className="sm:w-[192px] w-[100px] object-contain opacity-80 hover:opacity-100"
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+);
 
 export default Clients;
--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,5 +1,28 @@
-const FeedbackCard = () => {
-  return <div>FeedbackCard</div>;
-};
+import { quotes } from '@/assets';
+
+const FeedbackCard = ({ content, name, title, img }) => (
+  <div className="flex justify-between flex-col ss:px-10 px-7 ss:py-12 py-7 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+    <img
+      src={quotes}
+      alt="double quotes"
+      className="w-[42px] h-[27px] object-contain"
+    />
+    <p className="font-poppins font-normal text-[18px] leading-[32px] text-white my-10">
+      {content}
+    </p>
+    <div className="flex flex-row">
+      <img src={img} alt="name" className="w-[48px] h-[48px] rounded-full" />
+
+      <div className="flex flex-col ml-4">
+        <h4 className="font-poppins font-semibold text-[20px] leading-[32px] text-white">
+          {name}
+        </h4>
+        <p className="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite">
+          {title}
+        </p>
+      </div>
+    </div>
+  </div>
+);
 
 export default FeedbackCard;
